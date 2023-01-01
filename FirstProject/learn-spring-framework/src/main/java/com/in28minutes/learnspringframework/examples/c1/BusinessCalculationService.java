0928fd634d52c5a3ec3dcbbc318b14ce27ffa7c7
Marcus <marc9144@uni.sydney.edu.au>
2023-01-01 20:55:25 +1100

    Real world example using spring

---
+++ FirstProject/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/examples/c1/BusinessCalculationService.java
@@ -0,0 +1,27 @@
+package com.in28minutes.learnspringframework.examples.c1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class BusinessCalculationService {
+
+    private DataService dataService;
+
+    public BusinessCalculationService(DataService dataService) {
+        this.dataService = dataService;
+    }
+
+    public int findMax() {
+        return Arrays.stream(dataService.retrieveData())
+                .max().orElse(0);
+    }
+
+
+
+}
